02945e7b585e908615d3e6ad34ad937ddcda6382
CryDeTaan <CryDeTaan@users.noreply.github.com>
2017-03-05 08:14:47 +0200

    Update mysql_run_setup.sh

--- DEMP/mysql_run_setup.sh
+++ DEMP/mysql_run_setup.sh
@@ -1,9 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-# Run the MySQL setup script 
-chmod +x ${working_dir}/.mysql_run_setup.sh
-${working_dir}/.mysql_run_setup.sh
+# Run MySQL, initialise MySQL, and run the Install Script
 
+service mysql start
 
-# This script will launch the process manager tool and instantiate the processes within the container with each process running in its own pid
-/usr/bin/supervisord -n -c ${supervisor_conf}
+# Update the mysql_setup script with the environment variable details specified in the Dockerfile.
+sed -ie 's/database_name_here/'${APP_DB}'/g' ${working_dir}/.mysql_setup.sql
+sed -ie 's/mysql_server/'${MYSQL_ALLOWED_IP}'/g' ${working_dir}/.mysql_setup.sql
+sed -ie 's/mysql_user_here/'${APP_USER}'/g' ${working_dir}/.mysql_setup.sql
+sed -ie 's/mysql_password_here/'${APP_PASS}'/g' ${working_dir}/.mysql_setup.sql
+
+# Run the mysql_setup.sql to configure the new database permissions.
+mysql -u root < ${working_dir}/.mysql_setup.sql
+
+# During the installation the password was not set, here I update the root password with the environment variable, MYSQL_ROOT_PASS, set in the Dockerfile.
+mysqladmin -u root password ${MYSQL_ROOT_PASS}
+
+# Seeing that the environment variables contain sensitive information, I decided to unset it here so this can not be obtained. As mentioned in the readme file, this is probably not required as I intend on only using the stack to develop with, but should this be used by someone in a production environment, I'll have the comfort knowing that this is cleared out. 
+
+unset ${APP_DB},${MYSQL_ALLOWED_IP},${APP_USER},${APP_PASS},${MYSQL_ROOT_PASS}
+
+# I stop the mysql service here, as I will be instantiating it from supervisord. 
+service mysql stop
