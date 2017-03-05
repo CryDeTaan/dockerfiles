f78c256099525df0e526725b5f979a62541ce0b9
CryDeTaan <CryDeTaan@users.noreply.github.com>
2017-03-05 08:45:06 +0200

    Update mysql_run_setup.sh

--- DEMP/mysql_run_setup.sh
+++ DEMP/mysql_run_setup.sh
@@ -16,9 +16,10 @@ mysql -u root < ${working_dir}/.mysql_setup.sql
 # During the installation the password was not set, here I update the root password with the environment variable, MYSQL_ROOT_PASS, set in the Dockerfile.
 mysqladmin -u root password ${MYSQL_ROOT_PASS}
 
-# Seeing that the environment variables contain sensitive information, I decided to unset it here so this can not be obtained. As mentioned in the readme file, this is probably not required as I intend on only using the stack to develop with, but should this be used by someone in a production environment, I'll have the comfort knowing that this is cleared out. 
+# Seeing that the environment variables contain sensitive information, I decided to unset it here so this can not be obtained. As mentioned in the readme file, this is probably not required as I intend on only using the stack to develop with, but should this be used by someone in a production environment, I'll have the comfort knowing that this is cleared out.
+# The unset command does not work for some reason, so I just updated the environment variables with empty values.
 
-unset ${APP_DB},${MYSQL_ALLOWED_IP},${APP_USER},${APP_PASS},${MYSQL_ROOT_PASS}
+export APP_DB= && export MYSQL_ALLOWED_IP= && export APP_USER= && export APP_PASS= && export MYSQL_ROOT_PASS=
 
 # I stop the mysql service here, as I will be instantiating it from supervisord. 
 service mysql stop
