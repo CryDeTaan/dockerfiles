2825cf8a13298915563955597327ecf7c83da24b
CryDeTaan <CryDeTaan@users.noreply.github.com>
2017-03-05 08:17:06 +0200

    Update __init__.sh

--- DEMP/__init__.sh
+++ DEMP/__init__.sh
@@ -4,6 +4,9 @@
 chmod +x ${working_dir}/.mysql_run_setup.sh
 ${working_dir}/.mysql_run_setup.sh
 
+# Once I am done with these files, I'd prefer if they are removed as this contains sensitive details of the internal setup of MySQL. 
+
+rm -f ${working_dir}/.mysql*
 
 # This script will launch the process manager tool and instantiate the processes within the container with each process running in its own pid
 /usr/bin/supervisord -n -c ${supervisor_conf}
